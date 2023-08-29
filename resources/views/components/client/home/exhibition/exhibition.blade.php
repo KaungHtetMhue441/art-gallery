8c5e663ded49771ff342394320a5908438281fe2
Kaung Htet Hmue <kaunghethmuee@gmail.com>
2023-08-29 15:20:00 +0630

    fixed exhibition in home

--- resources/views/components/client/home/exhibition/exhibition.blade.php
+++ resources/views/components/client/home/exhibition/exhibition.blade.php
@@ -1,7 +1,8 @@
 
 <section class="container py-5 border-top">
     <x-client.common.title.center-title title="Upcoming Exhibition" />
-    
-    <x-client.exhibitions.exhibition-card :exhibition="$exhibitions[0]"  :shadow="false"/>
+    @if ($exhibitions->count()>0)
+        <x-client.exhibitions.exhibition-card :exhibition="$exhibitions[0]"  :shadow="false"/>
+    @endif
     
 </section>
